7b16c267d7c732da58ac3642cce579dc69851d61
Albertojg05 <alberto.jimenez252595@potros.itson.edu.mx>
2025-03-05 22:39:41 -0700

    Se agrega el "Análisis de Complejidad del Algoritmo"

--- AA_UC2_252595/src/Algoritmos/Ordenamiento.java
+++ AA_UC2_252595/src/Algoritmos/Ordenamiento.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Algoritmos;
 
 /**
@@ -11,16 +7,16 @@ package Algoritmos;
 public class Ordenamiento {
 
     public static void burbuja(int[] a) {
-        for (int i = 1; i < a.length - 1; i++) {
-            for (int j = 0; j < a.length - i; j++) {
+        for (int i = 1; i < a.length - 1; i++) { //(n - 1)
+            for (int j = 0; j < a.length - i; j++) { // (n - i)
                 // Comparación
-                if (a[j] > a[j + 1]) {
+                if (a[j] > a[j + 1]) { // (n - i)
                     // Intercambio
-                    int aux = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = aux;
-                }
-            }
-        }
-    }
+                    int aux = a[j]; // 1
+                    a[j] = a[j + 1]; // 1
+                    a[j + 1] = aux; // 1
+                } // (n - i)
+            } // (n - i)
+        } // (n - 1)
+    } // O(n^2)
 }
