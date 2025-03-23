1f7152503e82fe488773707c7a3911c68a2fcb5b
Albertojg05 <alberto.jimenez252595@potros.itson.edu.mx>
2025-03-23 12:40:14 -0700

    Se agrega el algoritmo Selection Sort a la clase Ordenamiento

--- AA_UC2_252595/src/Algoritmos/Ordenamiento.java
+++ AA_UC2_252595/src/Algoritmos/Ordenamiento.java
@@ -31,4 +31,19 @@ public class Ordenamiento {
             a[j+1] = key; // 1
         }
     } // O(n^2)
+    
+    public static void seleccion(int[] a){
+        int n = a.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if(a[j]<a[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int aux = a[minIndex];
+            a[minIndex] = a[i];
+            a[i] = aux;
+        }
+    }
 }
