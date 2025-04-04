c9c1f376761349b0efe0d4b657e7aa040be7e74b
Albertojg05 <alberto.jimenez252595@potros.itson.edu.mx>
2025-04-04 11:36:34 -0700

    Código corregido y análisis de complejidad

--- AA_UC2_252595/src/Algoritmos/Ordenamiento.java
+++ AA_UC2_252595/src/Algoritmos/Ordenamiento.java
@@ -19,25 +19,25 @@ public class Ordenamiento {
             } // (n - i)
         } // (n - 1)
     } // O(n^2)
-    
-    public static void insercion(int[] a){
+
+    public static void insercion(int[] a) {
         for (int i = 1; i < a.length; i++) { // (n-1)
             int key = a[i]; // 1
-            int j = i-1; // 1
-            while (j>=0 && a[j]>key) { // i
-                a[j+1] = a[j]; // 1
+            int j = i - 1; // 1
+            while (j >= 0 && a[j] > key) { // i
+                a[j + 1] = a[j]; // 1
                 j--; // 1
             }
-            a[j+1] = key; // 1
+            a[j + 1] = key; // 1
         }
     } // O(n^2)
-    
-    public static void seleccion(int[] a){
+
+    public static void seleccion(int[] a) {
         int n = a.length; // 1
-        for (int i = 0; i < n-1; i++) { // n
+        for (int i = 0; i < n - 1; i++) { // n
             int minIndex = i; // 1
-            for (int j = i+1; j < n; j++) { // n-1
-                if(a[j]<a[minIndex]){ // 1
+            for (int j = i + 1; j < n; j++) { // n-1
+                if (a[j] < a[minIndex]) { // 1
                     minIndex = j; // 1
                 }
             }
@@ -46,28 +46,30 @@ public class Ordenamiento {
             a[i] = aux; // 1
         } // 1+(n+1)+(n-1)+1+1+1+1+1
     } // O(n^2)
-    
-    public static int particionar(int[] a, int inicio, int fin){
-        int pivote = a[fin];
-        int i = a[inicio];
-        for(int actual = inicio; actual< fin; actual++){
-            if(a[actual] < pivote){
-                i++;
-                int aux = a[i];
-                a[i]=a[actual];
-                a[actual]=aux;
+
+    public static int particionar(int[] a, int inicio, int fin) {
+        int pivote = a[fin]; // 1
+        int i = inicio; // 1
+        for (int actual = inicio; actual < fin; actual++) { // n
+            if (a[actual] < pivote) { // 1
+                int aux = a[i]; // 1
+                a[i] = a[actual]; // 1
+                a[actual] = aux; // 1
+                i++; // 1
             }
         }
-        int temp = a[i];
-        a[i] = a[fin];
-        a[fin] = temp;
-        
-        return i;
+        int temp = a[i]; // 1
+        a[i] = a[fin]; // 1
+        a[fin] = temp; // 1
+
+        return i; // 1
     }
-    
-    public static void quickSort(int[] a, int inicio, int fin){
-        int pivote = particionar(a,inicio,fin);
-        quickSort(a, inicio, pivote-1);
-        quickSort(a, pivote+1, fin);
-    }
+
+    public static void quickSort(int[] a, int inicio, int fin) {
+        if (inicio < fin) { // 1.
+            int pivote = particionar(a, inicio, fin); // n
+            quickSort(a, inicio, pivote - 1); // n/2 
+            quickSort(a, pivote + 1, fin); // n/2
+        }
+    } // O(n^2)
 }
